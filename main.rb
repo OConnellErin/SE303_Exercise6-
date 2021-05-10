0f208f36fb16652eeb21e92f6c114162e1f83d9a
Erin O'Connell <oconerin@oregonstate.edu>
2021-05-09 22:39:53 -0700

    added comments

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
-def draw_button(label_text, x, y, foreground_color, is_dark_mode)
+def draw_button(label_text, x, y, foreground_color, is_dark_mode) #mysterious name smell. X and y are not great names that can be renamed more descriptively
+  #dataclumps smell on lines 1, 5, and 8. We could use the tools from extract class. 
   if is_dark_mode
-    # darken foreground color for dark mode
-    paint(label_text, x, y, foreground_color - 10, '#111111')
+    # darken foreground color for dark mode #unecessary comments. Could be taken out by abstracting a making a function called nondarkmode()
+    paint(label_text, x, y, foreground_color - 10, '#111111') #duplicated smell code mentioned below
   else
-    # lighten foreground color for non-dark mode
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+    # lighten foreground color for non-dark mode #unecessary comments. Could be taken out by abstracting a making a function called darkmode()
+    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')  #duplicated smell code, could be fixed with a function call paramed with the #value
   end
 end
